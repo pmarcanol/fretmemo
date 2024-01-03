f30f562f70871bb38859f7b6fe762304f516f289
BMarcano <brayanmarcanor@gmail.com>
2024-01-03 10:33:22 -0300

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { ModeScore } from "./ModeScore";
 
 function App() {
   return (
-    <div className="min-w-screen min-h-screen bg-gradient-to-b from-teal-800 to-teal-900 p-8">
+    <div className="min-w-screen min-h-screen bg-gradient-to-b from-teal-600 to-teal-800 p-8">
       <Header />
       <div className="text-white  flex flex-col justify-evenly">
         <div className="py-24">
@@ -18,6 +18,9 @@ function App() {
           <Score />
           <GameModeSelect />
         </div>
+        <div className="flex flex-row justify-center">
+          Made with ❤️ by <a className="no-underline hover:underline visited:text-white" href="https://www.pmarcano.com"> &nbsp; Pablo Marcano</a>
+          </div>
       </div>
     </div>
   );
